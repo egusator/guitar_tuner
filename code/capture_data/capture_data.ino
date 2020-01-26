b17b62ae8a30be188c842957dfbd6124f0e84270
Vladimir Chupin <vochupin@gmail.com>
2020-01-26 10:59:57 +0700

    добавил обработку реального сигнала в октаве + модуль для захвата данных и там определение частоты автокорреляцией

---
+++ code/capture_data/capture_data.ino
@@ -0,0 +1,114 @@
+#include <TM1638.h>
+
+// define a module on data pin 8, clock pin 9 and strobe pin 10
+TM1638 module(8, 9, 10);
+unsigned long a=1;
+
+#define BUF_LENGTH 512
+
+//data storage variables
+byte dataBuffer[BUF_LENGTH];
+int index = 0;//current storage index
+byte ready = 0;
+
+void setup(){
+  
+  Serial.begin(9600);
+  
+  cli();//disable interrupts
+  
+  //set up continuous sampling of analog pin 0 at 38.5kHz
+ 
+  //clear ADCSRA and ADCSRB registers
+  ADCSRA = 0;
+  ADCSRB = 0;
+  
+  ADMUX |= (1 << REFS0); //set reference voltage
+  ADMUX |= (1 << ADLAR); //left align the ADC value- so we can read highest 8 bits from ADCH register only
+  
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS0); //set ADC clock with 32 prescaler- 16mHz/32=500kHz
+  ADCSRA |= (1 << ADATE); //enabble auto trigger
+  ADCSRA |= (1 << ADIE); //enable interrupts when measurement complete
+  ADCSRA |= (1 << ADEN); //enable ADC
+  ADCSRA |= (1 << ADSC); //start ADC measurements
+  
+  sei();//enable interrupts
+}
+
+ISR(ADC_vect) {//when new ADC value ready
+
+  byte adc = ADCH;//get value from A0
+  
+  if (ready == 0) {
+    dataBuffer[index] = adc;
+
+    index++;
+    
+    if (index == BUF_LENGTH) {
+      index = 0;
+      ready = 1;
+    }
+  }
+  
+}
+
+
+
+void loop(){
+int i,k;
+long sum, sum_old;
+int thresh = 0;
+float freq_per = 0;
+byte pd_state = 0;
+
+  if (ready == 1) {
+//    for (int i = 0; i < BUF_LENGTH; i++) {
+//      Serial.print(dataBuffer[i]);
+//      Serial.print(", ");
+//    }
+//
+//    Serial.println(" ");
+
+    sum = 0;
+    pd_state = 0;
+    int period = 0;
+    for(i=0; i < BUF_LENGTH; i++)
+    {
+      // Autocorrelation
+      sum_old = sum;
+      sum = 0;
+      for(k=0; k < BUF_LENGTH-i; k++) sum += (dataBuffer[k]-128)*(dataBuffer[k+i]-128)/256;
+    /*  // RX8 [h=43] @1Key1 @0Key1
+      Serial.print("C");
+      Serial.write((rawData[i]-128)>>8);
+      Serial.write((rawData[i]-128)&0xff); */
+      
+    /*  // RX8 [h=43] @1Key1 @0Key1
+      Serial.print("C");
+      Serial.write(sum>>8);
+      Serial.write(sum&0xff); */
+      
+      // Peak Detect State Machine
+      if (pd_state == 2 && (sum-sum_old) <=0) 
+      {
+        period = i;
+        pd_state = 3;
+      }
+      if (pd_state == 1 && (sum > thresh) && (sum-sum_old) > 0) pd_state = 2;
+      if (!i) {
+        thresh = sum * 0.6;
+        pd_state = 1;
+      }
+    }
+    // Frequency identified in kHz
+    freq_per = 38400/period;
+    Serial.println(freq_per);
+    
+    if (freq_per > 60 && freq_per < 350 ) { 
+      module.setDisplayToDecNumber(freq_per,0,false);
+    }
+  
+    ready = 0;
+  };
+  delay(100);//delete this if you want
+}

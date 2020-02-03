41475b4bb8bd361c9c2bdb32d77516375269b786
Vladimir Chupin <vochupin@gmail.com>
2020-02-03 23:47:55 +0700

    попробовал функции из библиотеки TM1638

--- code/capture_data/capture_data.ino
+++ code/capture_data/capture_data.ino
@@ -52,75 +52,88 @@ ISR(ADC_vect) {//when new ADC value ready
   
 }
 
-
+int i = 0;
 
 void loop(){
-int i,k;
-long sum, sum_old;
-int thresh = 0;
-float freq_per = 0;
-byte pd_state = 0;
 
-  if (ready == 1) {
 
-    sum = 0;
-    sum_old = 0;
-    pd_state = 0;
-    int period = 0;
-    thresh = 0;
-    freq_per = 0;
+  module.setLED(TM1638_COLOR_RED, 1);
+  module.setLED(TM1638_COLOR_RED, 4);
 
-    for(i=0; i < BUF_LENGTH; i++)
-    {
-      // Autocorrelation
-      sum_old = sum;
-      sum = 0;
-      for(k=0; k < BUF_LENGTH-i; k++) sum += (dataBuffer[k]-128)*(dataBuffer[k+i]-128);
-    /*  // RX8 [h=43] @1Key1 @0Key1
-      Serial.print("C");
-      Serial.write((rawData[i]-128)>>8);
-      Serial.write((rawData[i]-128)&0xff); */
-      
-    /*  // RX8 [h=43] @1Key1 @0Key1
-      Serial.print("C");
-      Serial.write(sum>>8);
-      Serial.write(sum&0xff); */
-      
-      // Peak Detect State Machine
-      if (pd_state == 2 && (sum-sum_old) <= 0) 
-      {
-        period = i;
-        pd_state = 3;
-      }
-      if (pd_state == 1 && (sum > thresh) && (sum-sum_old) > 0) pd_state = 2;
-      if (!i) {
-        thresh = sum * 0.5;
-        pd_state = 1;
-      }
-    }
-    // Frequency identified in kHz
-    freq_per = 38400/period;
-    Serial.print(freq_per);
-    Serial.print(" ");
-    Serial.print(period);
-    Serial.print(" ");
-    Serial.println(thresh);
-
-    
-    if (freq_per > 60 && freq_per < 350 ) { 
-      module.setDisplayToDecNumber(freq_per,0,false);
-    }
-
-//    if (freq_per > 1000 && freq_per < 8000) {
-//      
-//      for (int i = 0; i < BUF_LENGTH; i++) {
-//        Serial.print(dataBuffer[i]);
-//        Serial.print(", ");
-//      }
-//      Serial.println(" ");
-//    }
+  i = module.getButtons();
   
-    ready = 0;
-  };
-  //delay(100);//delete this if you want
+  module.setDisplayToDecNumber(i,0,false);
+  delay(1000);
+  
 }
+
+
+//int i,k;
+//long sum, sum_old;
+//int thresh = 0;
+//float freq_per = 0;
+//byte pd_state = 0;
+//
+//  if (ready == 1) {
+//
+//    sum = 0;
+//    sum_old = 0;
+//    pd_state = 0;
+//    int period = 0;
+//    thresh = 0;
+//    freq_per = 0;
+//
+//    for(i=0; i < BUF_LENGTH; i++)
+//    {
+//      // Autocorrelation
+//      sum_old = sum;
+//      sum = 0;
+//      for(k=0; k < BUF_LENGTH-i; k++) sum += (dataBuffer[k]-128)*(dataBuffer[k+i]-128);
+//    /*  // RX8 [h=43] @1Key1 @0Key1
+//      Serial.print("C");
+//      Serial.write((rawData[i]-128)>>8);
+//      Serial.write((rawData[i]-128)&0xff); */
+//      
+//    /*  // RX8 [h=43] @1Key1 @0Key1
+//      Serial.print("C");
+//      Serial.write(sum>>8);
+//      Serial.write(sum&0xff); */
+//      
+//      // Peak Detect State Machine
+//      if (pd_state == 2 && (sum-sum_old) <= 0) 
+//      {
+//        period = i;
+//        pd_state = 3;
+//      }
+//      if (pd_state == 1 && (sum > thresh) && (sum-sum_old) > 0) pd_state = 2;
+//      if (!i) {
+//        thresh = sum * 0.5;
+//        pd_state = 1;
+//      }
+//    }
+//    // Frequency identified in kHz
+//    freq_per = 38400/period;
+//    Serial.print(freq_per);
+//    Serial.print(" ");
+//    Serial.print(period);
+//    Serial.print(" ");
+//    Serial.println(thresh);
+//
+//    
+//    if (freq_per > 60 && freq_per < 350 ) { 
+//      module.setDisplayToDecNumber(freq_per,0,false);
+//    }
+//
+////    if (freq_per > 1000 && freq_per < 8000) {
+////      
+////      for (int i = 0; i < BUF_LENGTH; i++) {
+////        Serial.print(dataBuffer[i]);
+////        Serial.print(", ");
+////      }
+////      Serial.println(" ");
+////    }
+//  
+//    ready = 0;
+//  };
+//  //delay(100);//delete this if you want
+//}

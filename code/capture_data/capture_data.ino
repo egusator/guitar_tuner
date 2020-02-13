9763af0ba97baf2573442671c23344026dfa1f6d
Vladimir Chupin <vochupin@gmail.com>
2020-02-13 23:15:34 +0700

    менюшка с кнопками

--- code/capture_data/capture_data.ino
+++ code/capture_data/capture_data.ino
@@ -1,11 +1,12 @@
 #include <TM1638.h>
 
-// define a module on data pin 8, clock pin 9 and strobe pin 10
+// назначим ножки для управления индикатором: данные - pin 8, синхра - pin 9 и строб pin 10
 TM1638 module(8, 9, 10);
 unsigned long a=1;
 
 #define BUF_LENGTH 512
 
+//Константы частот нот
 #define NOTE_e  329.63
 #define NOTE_B  246.94
 #define NOTE_G  196.00
@@ -13,8 +14,10 @@ unsigned long a=1;
 #define NOTE_A  110.00
 #define NOTE_E  82.41
 
+//Максимальное отклонение частоты которое будем настраивать
 #define MAX_DEVIATION 10
 
+//Максимальные и минимальные частоты для каждой ноты
 #define NOTE_e_MAX NOTE_e * (100 + MAX_DEVIATION) / 100
 #define NOTE_e_MIN NOTE_e * (100 - MAX_DEVIATION) / 100
 #define NOTE_B_MAX NOTE_B * (100 + MAX_DEVIATION) / 100
@@ -28,35 +31,53 @@ unsigned long a=1;
 #define NOTE_E_MAX NOTE_E * (100 + MAX_DEVIATION) / 100
 #define NOTE_E_MIN NOTE_E * (100 - MAX_DEVIATION) / 100
 
-#define BUTTON_S1 1
-#define BUTTON_S2 2
-#define BUTTON_S3 4
-#define BUTTON_S4 8
-#define BUTTON_S5 16
-#define BUTTON_S6 32
+//Маска бита кнопок
+#define BUTTON_S1               1
+#define BUTTON_S2               2
+#define BUTTON_S3               4
+#define BUTTON_S4               8
+#define BUTTON_S5               16
+#define BUTTON_S6               32
+#define BUTTON_FREQ             64
+#define BUTTON_UART             128
 
-#define LED_BIT_S1 0
-#define LED_BIT_S2 1
-#define LED_BIT_S3 2
-#define LED_BIT_S4 3
-#define LED_BIT_S5 4
-#define LED_BIT_S6 5
+//Номера светодиодов
+#define LED_BIT_S1              0
+#define LED_BIT_S2              1
+#define LED_BIT_S3              2
+#define LED_BIT_S4              3
+#define LED_BIT_S5              4
+#define LED_BIT_S6              5
+#define LED_BIT_FREQ            6
+#define LED_BIT_UART            7
 
-#define LED_MASK_S1 1
-#define LED_MASK_S2 2
-#define LED_MASK_S3 4
-#define LED_MASK_S4 8
-#define LED_MASK_S5 16
-#define LED_MASK_S6 32
+//Маски светодиодов
+#define LED_MASK_S1             1
+#define LED_MASK_S2             2
+#define LED_MASK_S3             4
+#define LED_MASK_S4             8
+#define LED_MASK_S5             16
+#define LED_MASK_S6             32
+#define LED_MASK_FREQ           64
+#define LED_MASK_UART           128
 
-#define STEP_MOTOR_ENABLE_PIN 4
-#define STEP_MOTOR_PULSE_PIN 3
-#define STEP_MOTOR_DIR_PIN 2
+//Ножки управления шаговым мотором
+#define STEP_MOTOR_ENABLE_PIN   4
+#define STEP_MOTOR_PULSE_PIN    3
+#define STEP_MOTOR_DIR_PIN      2
 
-#define STEP_MOTOR_ON 1
-#define STEP_MOTOR_OFF 0
-#define STEP_MOTOR_RIGHT 0
-#define STEP_MOTOR_LEFT 1
+//Константы состояния сигналов шагового мотора
+#define STEP_MOTOR_ON           1
+#define STEP_MOTOR_OFF          0
+#define STEP_MOTOR_RIGHT        0
+#define STEP_MOTOR_LEFT         1
+
+//Биты состояния частей программы
+#define STATE_UART_MESSAGES     1
+#define STATE_SHOW_FREQ         2
+
+#define ON                      1
+#define OFF                     0
 
 //data storage variables
 byte dataBuffer[BUF_LENGTH];
@@ -107,16 +128,113 @@ ISR(ADC_vect) {//when new ADC value ready
       ready = 1;
     }
   }
-
-  
 }
 
 float maxFreq;
 float minFreq;
 char indicatorBuffer[10];
 
+byte state;
+
+byte keys, oldKeys;
+word leds = 0;
+
 void loop(){
 
+
+  menu();
+ 
+  delay(100);
+  
+}
+
+void menu() {
+  boolean edit = false;
+  oldKeys = keys;
+  keys = module.getButtons();
+  
+  if (keys & BUTTON_S1) {
+    maxFreq = NOTE_e_MAX;
+    minFreq = NOTE_e_MIN;
+    
+    leds &= LED_MASK_FREQ | LED_MASK_UART;
+    leds |= LED_MASK_S1;
+    edit = true;
+  } else if (keys & BUTTON_S2) {
+    maxFreq = NOTE_B_MAX;
+    minFreq = NOTE_B_MIN;
+
+    leds &= LED_MASK_FREQ | LED_MASK_UART;
+    leds |= LED_MASK_S2;
+    edit = true;
+  } else if (keys & BUTTON_S3) {
+    maxFreq = NOTE_G_MAX;
+    minFreq = NOTE_G_MIN;
+
+    leds &= LED_MASK_FREQ | LED_MASK_UART;
+    leds |= LED_MASK_S3;
+    edit = true;
+  } else if (keys & BUTTON_S4) {
+    maxFreq = NOTE_D_MAX;
+    minFreq = NOTE_D_MIN;
+
+    leds &= LED_MASK_FREQ | LED_MASK_UART;
+    leds |= LED_MASK_S4;
+    edit = true;
+  } else if (keys & BUTTON_S5) {
+    maxFreq = NOTE_A_MAX;
+    minFreq = NOTE_A_MIN;
+
+    leds &= LED_MASK_FREQ | LED_MASK_UART;
+    leds |= LED_MASK_S5;
+    edit = true;
+  } else if (keys & BUTTON_S6) {
+    maxFreq = NOTE_E_MAX;
+    minFreq = NOTE_E_MIN;
+
+    leds &= LED_MASK_FREQ | LED_MASK_UART;
+    leds |= LED_MASK_S6;
+    edit = true;
+  } else if ((keys & BUTTON_FREQ) != 0 && (oldKeys & BUTTON_FREQ) == 0) {
+
+    state ^= STATE_SHOW_FREQ;
+
+    if (state & STATE_SHOW_FREQ) {
+      leds |= LED_MASK_FREQ;
+    } else {
+      leds &= ~LED_MASK_FREQ;
+    }
+     edit = true;
+ } else if ((keys & BUTTON_UART) != 0 && (oldKeys & BUTTON_UART) == 0) {
+
+    state ^= STATE_UART_MESSAGES;
+
+    if (state & STATE_UART_MESSAGES) {
+      leds |= LED_MASK_UART;
+    } else {
+      leds &= ~LED_MASK_UART;
+    }
+    edit = true;
+  }
+
+  if (edit) {
+    module.setLEDs(leds);
+
+//    dtostrf(maxFreq, 3, 0, indicatorBuffer);
+//    dtostrf(minFreq, 3, 0, indicatorBuffer + 5);
+//    indicatorBuffer[3] = ' ';
+//    indicatorBuffer[4] = ' ';
+//    
+//
+//    module.setDisplayToString(indicatorBuffer);
+
+  }
+
+}
+
+
+void turnTuner () {
+  
   digitalWrite(STEP_MOTOR_ENABLE_PIN, STEP_MOTOR_ON);
 
   digitalWrite(STEP_MOTOR_DIR_PIN, STEP_MOTOR_RIGHT);
@@ -136,63 +254,8 @@ void loop(){
     digitalWrite(STEP_MOTOR_PULSE_PIN, STEP_MOTOR_OFF);
     delay(1);
   }
-
-//  menu();
-// 
-//  delay(100);
-  
 }
 
-void menu() {
-    byte keys = module.getButtons();
-  word leds = 0;
-
-  if (keys & BUTTON_S1) {
-    maxFreq = NOTE_e_MAX;
-    minFreq = NOTE_e_MIN;
-    
-    leds |= LED_MASK_S1;
-  } else if (keys & BUTTON_S2) {
-    maxFreq = NOTE_B_MAX;
-    minFreq = NOTE_B_MIN;
-
-    leds |= LED_MASK_S2;
-  } else if (keys & BUTTON_S3) {
-    maxFreq = NOTE_G_MAX;
-    minFreq = NOTE_G_MIN;
-
-    leds |= LED_MASK_S3;
-  } else if (keys & BUTTON_S4) {
-    maxFreq = NOTE_D_MAX;
-    minFreq = NOTE_D_MIN;
-
-    leds |= LED_MASK_S4;
-  } else if (keys & BUTTON_S5) {
-    maxFreq = NOTE_A_MAX;
-    minFreq = NOTE_A_MIN;
-
-    leds |= LED_MASK_S5;
-  } else if (keys & BUTTON_S6) {
-    maxFreq = NOTE_E_MAX;
-    minFreq = NOTE_E_MIN;
-
-    leds |= LED_MASK_S6;
-  }
-
-  if (leds != 0) {
-    module.setLEDs(leds);
-
-    dtostrf(maxFreq, 3, 0, indicatorBuffer);
-    dtostrf(minFreq, 3, 0, indicatorBuffer + 5);
-    indicatorBuffer[3] = ' ';
-    indicatorBuffer[4] = ' ';
-    
-
-    module.setDisplayToString(indicatorBuffer);
-
-  }
-
-}
 
 //int i,k;
 //long sum, sum_old;
